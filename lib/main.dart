98958b1dada4f55be85dda7fc3dfca424a3b23e7
triipaathii <myself10121990@gmail.com>
2023-03-16 02:27:44 +0530

    firebase initiated

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'homepage.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+
 
   // This widget is the root of your application.
   @override
